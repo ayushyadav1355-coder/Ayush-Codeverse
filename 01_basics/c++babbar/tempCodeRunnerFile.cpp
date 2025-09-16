23b1a2a01ecbbb896140af85758b669f37f1702d
ayushyadav1355-coder <ayushyadav1355@gmail.com>
2025-09-17 01:09:34 +0530

    Add files via upload

---
+++ 01_basics/c++babbar/tempCodeRunnerFile.cpp
@@ -0,0 +1,26 @@
+int a = 1234;
+
+//     cout << a <<endl;
+
+//     char b = 'v';
+
+//     cout << b <<endl;
+
+//     bool bl = false;
+
+//     cout << bl <<endl;
+
+//     float f = 1.2;
+
+//     cout << f <<endl;
+
+//     double d = 12.33;
+
+//     cout << d <<endl;
+
+//     int size = sizeof(a);
+//     cout <<"size of a is:"<< size <<endl;
+        
+    
+
+// }
---
+++ 01_basics/datatypes.md
@@ -0,0 +1,6 @@
+# Object Types / Data Types
+
+- Number : 1234, 3.1415, 3+4j, 0b111, Decimal(), Fraction()
+- String :
+
+-list : 
